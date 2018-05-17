7da2d5be9b59f95dd0b8426797d6c38bddc889eb
ChimengSoso <meng741258@gmail.com>
2018-05-17 16:20:59 +0700

    plot graph

--- main.py
+++ main.py
@@ -15,13 +15,42 @@ dataset = dataset.drop(['data'], 1)
 
 for i in dataset:
     if dataset[i].dtypes == float:
-        dataset[i] = dataset[i].fillna(0)
+        dataset[i] = dataset[i].fillna(0.0)
 
 y = np.array(dataset['n_person'])
 dataset = dataset.drop(['n_person'], 1)
 
-print(y)
+# min_max_scaler = preprocessing.MinMaxScaler()
+# dataset[[i for i in dataset if dataset[i].dtype == float]] = min_max_scaler.fit_transform(dataset[[i for i in dataset if dataset[i].dtypes == float]])
 
-print(dataset)
+# x = np.array(dataset)
 
-# plt.plot()
+x = [np.array(dataset[i]) for i in dataset]
+
+plt.plot(x[0], y, 'yo', x[1], y, 'ro', x[2], y, 'go', x[3], y, 'bo')
+
+# plt.axis([1, 40, 0, 2000])
+plt.xlabel('Tempereture')
+plt.ylabel('N person')
+plt.title('Rate of Number Population')
+
+plt.show()
+
+# print(x)
+# print(y)
+
+# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+
+# lr = LinearRegression()
+# lr.fit(x_train, y_train)
+
+# prediction = lr.predict(x_test)
+
+# print("Prediction : ")
+# print(prediction)
+
+# print("\nSould be happen:")
+# print(y_test)
+
+# print("\nR2 score:")
+# print(r2_score(prediction, y_test))
